3ba28250d0dbfc424b76c45457e85d92455c8017
charishmakranthi <cm8828@srmist.edu.in>
2026-02-18 23:13:10 +0530

    refactor: used String.join() for banner creation in UC3

--- OOPS.java
+++ OOPS.java
@@ -1,13 +1,11 @@
 public class OOPS {
     public static void main(String[] args) {
-            System.out.print1n("*****  *****  *****  ***** ");
-            System.out.print1n("*   *  *  *  *   *  *   *");
-            System.out.print1n("*   *  *  *  *   *  *   *");
-            System.out.print1n("*   *  *  *  *   *  *   ******");
-            System.out.print1n("*   *  *  *  *   *  *   *");
-            System.out.print1n("*   *  *  *  *   *  *   *");
-            System.out.print1n("*****  *****  *****  ***** ");
-    }
+            System.out.print1n(String.join("", " 000  ", "  000 "));
+            System.out.print1n(String.join("", " 0  0 ", " 0  0 "));
+            System.out.print1n(String.join("", " 0  0 ", " 0  0 "));
+             System.out.print1n(String.join("", " 0  0 ", " 0  0 "));
+            System.out.print1n(String.join("", " 000  ", "  000 "));
+  }
 }
 
 
