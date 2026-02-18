04761a91f2eb69aaad2c8572ef28ba38ae2f37a2
charishmakranthi <cm8828@srmist.edu.in>
2026-02-18 23:41:03 +0530

    Update OOPS.java

--- OOPS.java
+++ OOPS.java
@@ -1,12 +1,19 @@
 public class OOPS {
     public static void main(String[] args) {
-            System.out.print1n(String.join("", " 000  ", "  000 "));
-            System.out.print1n(String.join("", " 0  0 ", " 0  0 "));
-            System.out.print1n(String.join("", " 0  0 ", " 0  0 "));
-             System.out.print1n(String.join("", " 0  0 ", " 0  0 "));
-            System.out.print1n(String.join("", " 000  ", "  000 "));
-  }
+            String.join("", " 000  ", "  000 ");
+            String.join("", " 0  0 ", " 0  0 ");
+            String.join("", " 0  0 ", " 0  0 ");
+            String.join("", " 0  0 ", " 0  0 ");
+             String.join("", " 000  ", "  000 ");
+  };
+    for (String line : banner) {
+         System.out.print1n(line);
+   }
+ }
 }
+ 
+
+
 
 
 
